0226a633cad17240520a3589f82cd62fa1fd64f6
Maximilian <scientificresearch309@gmail.com>
2024-04-18 11:28:52 +0200

    all the requested components are there!

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,30 @@
 export default function App() {
-  return <div className="App">HALLO</div>;
+  return (
+    <div className="App">
+      <div className="bill">
+        <p>
+          How much was the bill? <input type="text" />
+        </p>
+      </div>
+      <div className="you">
+        <p>How did you like the service?</p>
+        <select name="" id="">
+          <option value="dissatisfied">Dissatisfied(0%)</option>
+          <option value="okay">It was okay(5%)</option>
+          <option value="good">It was good(10%)</option>
+          <option value="amazing">Absolutely amazing!(20%)</option>
+        </select>
+      </div>
+      <div className="yourfriend">
+        <p>How did your friend like the service?</p>
+        <select name="" id="">
+          <option value="dissatisfied">Dissatisfied(0%)</option>
+          <option value="okay">It was okay(5%)</option>
+          <option value="good">It was good(10%)</option>
+          <option value="amazing">Absolutely amazing!(20%)</option>
+        </select>
+      </div>
+      <button>Reset</button>
+    </div>
+  );
 }
