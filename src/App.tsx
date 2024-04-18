66e51b815f15e620fe5ca591ec260fa47f7ca30f
Maximilian <scientificresearch309@gmail.com>
2024-04-19 01:05:45 +0200

    and now, everything is working but the Typescript has to be corrected!

--- src/App.tsx
+++ src/App.tsx
@@ -1,129 +1,119 @@
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 
 export default function App() {
-  let yourSatisfactionResult = 0;
-  let friendSatisfactionResult = 0;
-  let calculatedYourSatisfaction = 0;
-  let calculatedFriendSatisfaction = 0;
-  let totalAmount = 0;
-  let averageTip = 0;
-
-  const [billValue, setBillValue] = useState<number>(0);
-  const [yourSatisfaction, setYourSatisfaction] = useState("dissatisfied");
-  const [friendSatisfaction, setFriendSatisfaction] = useState("dissatisfied");
-
-  const handleBillValue = (e: ChangeEvent<HTMLInputElement>) => {
-    const billAmount = parseInt(e.target.value);
-    setBillValue(() => billAmount);
-  };
-
-  // const handleYourSatisfaction = (e: ChangeEvent<HTMLSelectElement>) => {
-  const handleYourSatisfaction = () => {
-    // const yourSatisfaction = e.target.value;
-    // setYourSatisfaction(() => yourSatisfaction);
-
-    switch (yourSatisfaction) {
-      case "dissatisfied":
-        yourSatisfactionResult = 0 * billValue;
-        break;
-      case "okay":
-        yourSatisfactionResult = 0.05 * billValue;
-        break;
-      case "good":
-        yourSatisfactionResult = 0.1 * billValue;
-        break;
-      case "amazing":
-        yourSatisfactionResult = 0.2 * billValue;
-        break;
-    }
-    return yourSatisfactionResult;
-  };
-
-  // const handleFriendSatisfaction = (e: ChangeEvent<HTMLSelectElement>) => {
-  const handleFriendSatisfaction = () => {
-    // const friendSatisfaction = e.target.value;
-    // setFriendSatisfaction(() => friendSatisfaction);
-
-    switch (friendSatisfaction) {
-      case "dissatisfied":
-        friendSatisfactionResult = 0 * billValue;
-        break;
-      case "okay":
-        friendSatisfactionResult = 0.05 * billValue;
-        break;
-      case "good":
-        friendSatisfactionResult = 0.1 * billValue;
-        break;
-      case "amazing":
-        friendSatisfactionResult = 0.2 * billValue;
-        break;
-    }
-    return friendSatisfactionResult;
-  };
-
-  // call the both functions
-  calculatedYourSatisfaction = handleYourSatisfaction();
-  calculatedFriendSatisfaction = handleFriendSatisfaction();
-
-  averageTip = Number((
-    (calculatedYourSatisfaction + calculatedFriendSatisfaction) /
-    2
-  ).toFixed(2));
-  totalAmount = billValue + averageTip;
-
-  // Reset the values
-  const handleReset = () => {
-    setBillValue(() => 0);
-    setYourSatisfaction(() => "");
-    setFriendSatisfaction(() => "");
-  };
-
   return (
     <>
-      <div className="App">
-        <div className="bill">
-          <p>
-            How much was the bill?
-            <input
-              type="number"
-              value={billValue}
-              onChange={(e) => handleBillValue(e)}
-            />
-          </p>
-        </div>
-        <div className="you">
-          <p>How did you like the service?</p>
-          <select
-            name=""
-            id=""
-            value={yourSatisfaction}
-            // onChange={(e) => handleYourSatisfaction(e)}
-            onChange={(e) => setYourSatisfaction(e.target.value)}
-          >
-            <option value="dissatisfied">Dissatisfied(0%)</option>
-            <option value="okay">It was okay(5%)</option>
-            <option value="good">It was good(10%)</option>
-            <option value="amazing">Absolutely amazing!(20%)</option>
-          </select>
-        </div>
-        <div className="yourfriend">
-          <p>How did your friend like the service?</p>
-          <select
-            name=""
-            id=""
-            value={friendSatisfaction}
-            // onChange={(e) => handleFriendSatisfaction(e)}
-            onChange={(e) => setFriendSatisfaction(e.target.value)}
-          >
-            <option value="dissatisfied">Dissatisfied(0%)</option>
-            <option value="okay">It was okay(5%)</option>
-            <option value="good">It was good(10%)</option>
-            <option value="amazing">Absolutely amazing!(20%)</option>
-          </select>
-        </div>
-        <p>{`You pay $${totalAmount} ($${billValue} + $${averageTip} tip )`}</p>
-        <button onClick={handleReset}>Reset</button>
+      <div>
+        <TipCalculator />
       </div>
     </>
   );
 }
+const TipCalculator = () => {
+  const [billValue, setBillValue] = useState<number>(0);
+  // const [yourSatisfaction, setYourSatisfaction] = useState("dissatisfied");
+  const [yourSatisfaction, setYourSatisfaction] = useState(0);
+  // const [friendSatisfaction, setFriendSatisfaction] = useState("dissatisfied");
+  const [friendSatisfaction, setFriendSatisfaction] = useState(0);
+
+  // NOTE: every time when we set a new value in the input fields, this value will be set in the state variables and therefore, the new values will be calculated! In my calculations, i used the name of every selection like "It was good" and not its value => 10%, that's why i had to use switch-case and get the corresponding value for every selection. But now, we use the corresponding value for every selection directly using state variable!
+  const tip = billValue * ((yourSatisfaction + friendSatisfaction) / 2 / 100);
+
+  return (
+    <>
+      <BillInput billValue={billValue} onSetBillValue={setBillValue} />
+      <SelectPercentage
+        satisfactionPercentage={yourSatisfaction}
+        onSatisfactionPercentage={setYourSatisfaction}
+      >
+        How did you like the service?
+      </SelectPercentage>
+      <SelectPercentage
+        satisfactionPercentage={friendSatisfaction}
+        onSatisfactionPercentage={setFriendSatisfaction}
+      >
+        How did your friend like the service?
+      </SelectPercentage>
+      <Output billValue={billValue} tip={tip} />
+      <Reset
+        onSetBillValue={setBillValue}
+        onSatisfactionPercentage={setFriendSatisfaction}
+      />
+    </>
+  );
+};
+
+const BillInput = ({
+  billValue,
+  onSetBillValue,
+}: {
+  billValue: any;
+  onSetBillValue: any;
+}) => {
+  return (
+    <>
+      <div className="bill">
+        <label htmlFor="">How much was the bill?</label>
+        <input
+          type="number"
+          placeholder="Bill value ..."
+          value={billValue}
+          onChange={(e) => onSetBillValue(Number(e.target.value))}
+        />
+      </div>
+    </>
+  );
+};
+
+const SelectPercentage = ({
+  children,
+  satisfactionPercentage,
+  onSatisfactionPercentage,
+}: {
+  children: any;
+  satisfactionPercentage: any;
+  onSatisfactionPercentage: any;
+}) => {
+  return (
+    <div className="you">
+      <label>{children}</label>
+      <select
+        value={satisfactionPercentage}
+        onChange={(e) => onSatisfactionPercentage(Number(e.target.value))}
+      >
+        {/* <option value="dissatisfied">Dissatisfied(0%)</option> */}
+        <option value="0">Dissatisfied(0%)</option>
+        {/* <option value="okay">It was okay(5%)</option> */}
+        <option value="5">It was okay(5%)</option>
+        {/* <option value="good">It was good(10%)</option> */}
+        <option value="10">It was good(10%)</option>
+        {/* <option value="amazing">Absolutely amazing!(20%)</option> */}
+        <option value="20">Absolutely amazing!(20%)</option>
+      </select>
+    </div>
+  );
+};
+
+const Output = ({ billValue, tip }: { billValue: any; tip: any }) => {
+  return (
+    <h3>
+      You pay ${billValue + tip} (${billValue} + ${tip} tip)
+    </h3>
+  );
+};
+
+const Reset = ({
+  onSetBillValue,
+  onSatisfactionPercentage,
+}: {
+  onSetBillValue: any;
+  onSatisfactionPercentage: any;
+}) => {
+  // Reset the values
+  const handleReset = () => {
+    onSetBillValue(() => 0);
+    onSatisfactionPercentage(() => "");
+  };
+
+  return <button onClick={handleReset}>Reset</button>;
+};
